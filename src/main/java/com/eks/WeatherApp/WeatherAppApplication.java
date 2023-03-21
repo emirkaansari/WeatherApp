869798a8c150f80486e7c84888c5fd5dd2456a24
Emir <emirkaansari@gmail.com>
2023-03-21 16:54:54 +0200

    scheduled task update

--- src/main/java/com/eks/WeatherApp/WeatherAppApplication.java
+++ src/main/java/com/eks/WeatherApp/WeatherAppApplication.java
@@ -1,8 +1,10 @@
 package com.eks.WeatherApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeatherAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherAppApplication.class, args);
--- src/main/java/com/eks/WeatherApp/service/WeatherService.java
+++ src/main/java/com/eks/WeatherApp/service/WeatherService.java
@@ -4,6 +4,8 @@ package com.eks.WeatherApp.service;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.eks.WeatherApp.pojo.Forecasts;
@@ -13,7 +15,9 @@ import java.io.IOException;
 import java.net.URL;
 @Service
 public class WeatherService {
+    @Scheduled(fixedRate = 3600000)
     public Forecasts getData () throws JAXBException, IOException {
+        System.out.println("Scheduled task running...");
         URL url = new URL("https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng");
 
         JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
